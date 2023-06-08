83eaed623044b919e8da89d93d94c066999aa99b
an0mal1a <129337574+an0mal1a@users.noreply.github.com>
2023-06-08 21:24:09 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="evilHunter",
+    version="0.1.2",
+    description="Cracking WiFi(Hanshake)",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="an0mal1a",
+    url="https://github.com/an0mal1a/evilHunter",
+    author_email="pablodiez024@proton.me",
+    packages=["words"],
+    scripts=["evilHunter.py"],
+    install_requires=[
+        'aircrack-ng'
+],
+
+)
